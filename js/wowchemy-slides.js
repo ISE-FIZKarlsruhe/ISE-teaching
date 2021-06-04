8cd26d6969bfbc66adfef3db707c539405745b96
anntanp <anntanp@gmail.com>
2021-06-04 10:26:03 +0200

    added lecture 8 video link

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/var/folders/xd/q1t79mh96y9cc9b_0pjzlzth0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210516101231-84e462a5a149/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/xd/q1t79mh96y9cc9b_0pjzlzth0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210602222123-e9d82a242153/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
